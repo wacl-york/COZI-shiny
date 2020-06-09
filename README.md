a8ebb80d8446364ebd71caa2adb05add729a5128
Stuart Lacy <stuart.lacy@gmail.com>
2020-06-09 17:04:11 +0100

    Initial release (#1)
    * Basic improvements
    
    Added a spinner when plots are loading, made a separate About page, added boiler plate code for a footer
    
    * Added "loading data" message
    
    * Changed way plots are rendered
    
    Previously all plots were re-generated when an input (either measurand selection or date range) was changed, causing unnecessary loading times.
    
    Now, all plots are generated when the date range is changed, and hidden/displayed by CSS when the measurand checkboxes are interacted with. This results in instantaneous addition/removal of measurand plots with minimal additional overhead
    
    * Added disclaimer + stylesheet
    
    * Spinner correctly stops when plots are ready
    
    * Removed missing values warning
    
    * Don't commit data
    
    * Added logos
    
    Logos have been temporarily added to the header, although it is likely they will reside elsewhere in the final version so that they are more readable
    
    * Updated TODO

--- README.md
+++ README.md
@@ -2,17 +2,20 @@
 
 This repository contains a Shiny web-application that displays the COZI instrumentation data.
 
-# Dependencies
+## Dependencies
 
 It requires a modern version of R (it has been tested on 4.0.0 but should work on 3.4.x) and the following packages:
 
   - `dplyr`
   - `shiny`
   - `shinydashboard`
+  - `shinyjs`
+  - `shinycssloaders`
   - `lubridate`
+  - `ggplot2`
   
 It also requires the data to be available in the file "data/data.csv", although in future releases this path may be configurable.
   
-# Installation
+## Installation
 
 Clone the repository into a folder that is read by the Shiny web service.
--- ui.R
+++ ui.R
@@ -1,31 +1,129 @@
 library(shiny)
+library(shinyjs)
 library(shinydashboard)
+library(shinycssloaders)
 
-disclaimer <- "<h3>Disclaimer</h3>This is raw uncalibrated reference data, please contact <a href='https://www.york.ac.uk/chemistry/staff/eotechs/kread/'>Katie Read</a> for further details."
+# Replaces the script used in shinycssloaders::withSpinner with our edited version
+modified_spinner <- function(input_tags) {
+    input_tags[[2]] <- tags$script(src="spinner_modified.js")
+    input_tags
+}
 
-# define ui for application that draws a histogram
-ui <- dashboardPage(
+about_text <- "
+<p>
+This is air quality data from the National Centre for Atmospheric Science (NCAS) COZI* Laboratory at the <a href='https://www.york.ac.uk/chemistry/research/wacl/'>Wolfson Atmospheric Chemistry Laboratories (WACL)</a>, University of York.  
+The instruments are housed in COZI and the measurements are made from the WACL rooftop at ~10m through a 1 inch sampling manifold. The data shown here is not final but raw instrument data, and so it may therefore be subject to further calibrations. In addition the COZI is a working instrument characterisation facility and so gaps in the measurements may be necessary when instruments are being used for other routine work.  
+</p>
+<p>
+Please contact <a href='mailto:katie.read@ncas.ac.uk'>Katie Read</a> for more information about these measurements or what the <a href='https://amof.ac.uk/laboratory/carbon-monoxide-and-ozone-calibration-laboratory-cozi/ '>COZI has to offer the community</a>.
+Meteorological data from the same location is also available on request.
+</p>
+<p>
+*COZI - Characterisation (e.g. of Ozone measurements) and Intercomparison Laboratory 
+</p>
+"
+footer <- tags$footer("Footer", align = "center", 
+             style = "
+                      * {
+                          margin: 0;
+                      }
+                      html, body {
+                          height: 100%;
+                      }
+                      .wrapper { min-height: 100%; margin: 0 auto -300px; /* the bottom margin is the negative value of the footer's height */
+                      }
+                      .footer, .push {
+                        height: 300px; /* .push must be the same height as .footer */
+                      }
+                    
+                      /*
 
-    dashboardHeader(title = "COZI Reference Data"),
-    
-    dashboardSidebar(
-        sidebarMenu(
-            radioButtons(
-                "daterange",
-                "Date range:",
-                c("Previous week" = "week",
-                  "All data" = "all")
+                      Sticky Footer by Ryan Fait
+                      http://ryanfait.com/
+                    
+                      */"
+                      )
+
+
+sidebar <- dashboardSidebar(
+    sidebarMenu(
+        menuItem("Live data", tabName = "dashboard", icon = icon("chart-bar")),
+        menuItem("About", icon = icon("info"), tabName = "about")
+    )
+)
+
+body <- dashboardBody(
+    useShinyjs(),
+    tabItems(
+        tabItem(tabName = "dashboard",
+                div(
+                    id="loading_page",
+                        h1("Loading data...", align="center")
+                ),
+                hidden(
+                    div(
+                        id="main_content",
+                        fluidRow(
+                                radioButtons(
+                                    "daterange",
+                                    "Date range",
+                                    c("Previous week" = "week",
+                                      "All data" = "all"),
+                                    inline=TRUE,
+                                    width="100%",
+                                ),
+                            align="center"
+                        ),
+                        fluidRow(
+                            uiOutput("selectmeasurands"),
+                            align="center",
+                        ),
+                        # Point the spinner JS at our modified version that correctly
+                        # doesn't stop spinning before the plots are loaded
+                        modified_spinner(withSpinner(uiOutput("plotui"),
+                                                     color = "#28a745"))
+                    )
+                )
+        ),
+        tabItem(tabName = "about",
+                div(id="about",
+                    h2("About"),
+                    HTML(about_text),
+                )
+        )
+    )
+)
+
+# Put them together into a dashboardPage
+ui <- tagList(
+        tags$head(
+            tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+        ),
+        dashboardPage(
+                dashboardHeader(title = "COZI Reference Data",
+                                tags$li(a(href = 'https://www.york.ac.uk/chemistry/research/wacl/',
+                                          img(src = 'logos/wacl.png',
+                                              title = "WACL", height = "30px"),
+                                          style = "padding-top:10px; padding-bottom:10px;"),
+                                        class = "dropdown"),
+                                tags$li(a(href = 'https://www.york.ac.uk/',
+                                          img(src = 'logos/uoy.png',
+                                              title = "University of York", height = "30px"),
+                                          style = "padding-top:10px; padding-bottom:10px;"),
+                                        class = "dropdown"),
+                                tags$li(a(href = 'https://amof.ac.uk/',
+                                          img(src = 'logos/amof.png',
+                                              title = "AMOF", height = "30px"),
+                                          style = "padding-top:10px; padding-bottom:10px;"),
+                                        class = "dropdown"),
+                                tags$li(a(href = 'https://www.ncas.ac.uk/en/',
+                                          img(src = 'logos/ncas.png',
+                                              title = "NCAS", height = "30px"),
+                                          style = "padding-top:10px; padding-bottom:10px;"),
+                                        class = "dropdown")),
+                sidebar,
+                body,
+                skin="green"
             ),
-            uiOutput("selectmeasurands")
-        )
-    ),
-    
-    dashboardBody(
-        fluidRow(
-            #plotOutput("timeseries", height = "800px"),
-            uiOutput("plotui"),
-            HTML(disclaimer)
-        )
-    ),
-    skin="green"
+        #footer TODO This is how can add footer when/if needed
 )
